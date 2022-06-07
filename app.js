c17ae0fdbcbf4ac01352cc94ff26a085d4176d06
unknown <nauerm@gmail.com>
2022-06-07 13:46:48 -0300

    Updated widths and statistics button

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
 
-            function ShowExercises() {
-                document.getElementById("myDropdown").classList.toggle("show");
-              }
-              // Close the dropdown if the user clicks outside of it
-              window.onclick = function(event) {
-              if (!event.target.matches('.dropbtn')) {
-                  var dropdowns = document.getElementsByClassName("dropdown-content");
-                  var i;
-                  for (i = 0; i < dropdowns.length; i++) {
-                  var openDropdown = dropdowns[i];
-                  if (openDropdown.classList.contains('show')) {
-                      openDropdown.classList.remove('show');
-                  }
-                  }
-              }
-              }
-              const GSheetReader = require('g-sheets-api');
+function ShowExercises()
+ {
+    document.getElementById("ExerciseDropdown").classList.toggle("show");
+    }
+    // Close the dropdown if the user clicks outside of it
+    window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+        }
+        }
+    }
+}
+
+function ShowStats()
+ {
+    document.getElementById("StatsDropdown").classList.toggle("show");
+    }
+    // Close the dropdown if the user clicks outside of it
+    window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+        }
+        }
+    }
+}
+    const GSheetReader = require('g-sheets-api');
